1c32677a827dd755db1def3216a26540075cda56
PhatNguyen24 <daiphat2442003@gmail.com>
2024-05-10 17:31:58 +0700

    connect-mysql-aiven

--- Model/aivenDbConnection.php
+++ Model/aivenDbConnection.php
@@ -1,6 +1,6 @@
 <?php
 
-$uri = "mysql://avnadmin:AVNS_5Ych6k-xoWyh12cOlnI@mysql-2eb27325-insourceit-0c39.h.aivencloud.com:17610/defaultdb?ssl-mode=REQUIRED";
+$uri = "mysql://avnadmin:AVNS_pAW2BeglFY8_fkCunMu@mysqldb-webncdb.h.aivencloud.com:20343/defaultdb?ssl-mode=REQUIRED";
 
 $fields = parse_url($uri);
 
@@ -8,32 +8,22 @@ $fields = parse_url($uri);
 $conn = "mysql:";
 $conn .= "host=" . $fields["host"];
 $conn .= ";port=" . $fields["port"];;
-$conn .= ";dbname=defaultdb";
-$conn .= ";sslmode=verify-ca;sslrootcert='pri/ca.pem'";
+$conn .= ";dbname=QUANLYSACH";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
 
 try {
-    $db = new PDO($conn, $fields["user"], $fields["pass"]);
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
 
-    $stmt = $db->query("SELECT version()");
-
-    print($stmt->fetch()[0]);
-    $row = $db->query("select * from todos");
-
-    /*printf($row->rowCount());
-    if($row->rowCount()>0){
-        while ($r = $row->fetch()[2]){
-            echo $r;
-
-        }
-    }*/
+//   $stmt = $db->query("SELECT VERSION()");
+    $row = $db->query("select * from User");
+//   print($stmt->fetch()[0]);
     if($row->rowCount()>0){
         while ($r = $row->fetchObject()){
-            echo $r->date;
-            echo $r->description;
+            echo $r->TenUser;
+            echo $r->MatKhau;
 
         }
     }
-   
 } catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
+  echo "Error: " . $e->getMessage();
 }
--- index.php
+++ index.php
@@ -1,29 +1,17 @@
 <?php
 
-$uri = "mysql://avnadmin:AVNS_pAW2BeglFY8_fkCunMu@mysqldb-webncdb.h.aivencloud.com:20343/defaultdb?ssl-mode=REQUIRED";
+?>
 
-$fields = parse_url($uri);
-
-// build the DSN including SSL settings
-$conn = "mysql:";
-$conn .= "host=" . $fields["host"];
-$conn .= ";port=" . $fields["port"];;
-$conn .= ";dbname=QUANLYSACH";
-$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
-
-try {
-  $db = new PDO($conn, $fields["user"], $fields["pass"]);
-
-//   $stmt = $db->query("SELECT VERSION()");
-    $row = $db->query("select * from User");
-//   print($stmt->fetch()[0]);
-    if($row->rowCount()>0){
-        while ($r = $row->fetchObject()){
-            echo $r->TenUser;
-            echo $r->MatKhau;
-
-        }
-    }
-} catch (Exception $e) {
-  echo "Error: " . $e->getMessage();
-}
+<html>
+	<head>
+		<title>Advanced Web Development</title>
+	</head>
+	<body>
+		<p style="color: black;">Login Form</p>
+		<form action="Controller/checkLogin.php" method="POST">
+		<a>Username:</a><input type=text name="userName" size =16>
+		<a>Password:</a><input type=password name="passWord" size =16>
+		<input type=submit name=submit value="Login">
+		</form>
+	</body>
+</html>
